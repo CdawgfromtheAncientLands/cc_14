f1293e775bfdc81147461de5412b7c3141c7d15b
CdawgfromtheAncientLands <cmrbusiness123456@gmail.com>
2024-06-29 17:11:00 -0400

    Implement interest rate calculation module.

---
+++ cc_14-script.js
@@ -0,0 +1,4 @@
+//U9414-0233
+import {calculateInterest} from "./interest";
+import {calculateLoanPayment} from "./loanpayment";
+import {calculateInvestmentReturn} from "./investmentreturn";
---
+++ interest.js
@@ -0,0 +1,3 @@
+export function calculateInterest(principal, rate, yearsInvested) {
+    return principal * rate * yearsInvested
+};
